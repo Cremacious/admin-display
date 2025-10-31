218b1868202261041de497b568af92fa3eb47933
cremacious <chrismackall3@gmail.com>
2025-10-31 10:24:02 -0400

    Created products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from './components/common/Sidebar';
 import OverviewPage from './pages/OverviewPage';
+import ProductsPage from './pages/ProductsPage';
+// import UsersPage from './pages/UsersPage';
+// import SalesPage from './pages/SalesPage';
+// import OrdersPage from './pages/OrdersPage';
+// import AnalyticsPage from './pages/AnalyticsPage';
+// import SettingsPage from './pages/SettingsPage';
 
 function App() {
   return (
@@ -13,8 +19,8 @@ function App() {
       <Sidebar />
       <Routes>
         <Route path="/" element={<OverviewPage />} />
-        {/* <Route path='/products' element={<ProductsPage />} />
-				<Route path='/users' element={<UsersPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        {/* <Route path='/users' element={<UsersPage />} />
 				<Route path='/sales' element={<SalesPage />} />
 				<Route path='/orders' element={<OrdersPage />} />
 				<Route path='/analytics' element={<AnalyticsPage />} />
